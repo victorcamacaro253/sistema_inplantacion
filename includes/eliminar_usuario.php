5cf71c421c3b93291822f6ea8f468c377e47808a
Victor C <74551928+victorcamacaro253@users.noreply.github.com>
2023-01-08 23:53:22 -0400

    Add files via upload

---
+++ includes/eliminar_usuario.php
@@ -0,0 +1,18 @@
+<?php
+
+$conexion=new mysqli('localhost','root','','implantacion');
+session_start();
+$id_usuario=$_GET['id'];
+
+$sql="DELETE  FROM usuarios WHERE Id_usuario='$id_usuario'";
+
+$query=$conexion->query($sql);
+if ($query) {
+	echo "<script>alert('Actualizado con exito')
+	              window.location.href='../vistas/gestionar_usuarios.php'</script>";
+}
+
+
+
+
+?>
---
+++ includes/getlocalidad.php
@@ -0,0 +1,32 @@
+<?php
+
+
+
+$mysqli=new mysqli('localhost','root','','implantacion');
+
+
+/*$id_municipio=$_POST['id_municipio'];
+
+$query="SELECT * FROM parroquias where id_municipio='$id_municipio'";
+$resultado=$mysqli->query($query);
+
+while ($row=$resultado->fetch_assoc()) {
+	$html.="<option value='".$row['id_parroquia']."'>".$row['parroquia']."</option>";
+}
+
+echo $html;*/
+
+
+$id_municipio = $_POST['id_municipio'];
+	
+	$query = "SELECT * FROM parroquias WHERE id_municipio = '$id_municipio'";
+	$resultado=$mysqli->query($query);
+	$html="<option value='0'>seleccionar parroquia</option>";
+	while($row = $resultado->fetch_assoc())
+	{
+		$html.= "<option value='".$row['id_parroquia']."'>".$row['parroquia']."</option>";
+	}
+	echo $html;
+
+
+?>
