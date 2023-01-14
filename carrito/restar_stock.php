fcbde4ee08842b3faa603d112553fbdfd31f29ae
Victor C <74551928+victorcamacaro253@users.noreply.github.com>
2023-01-14 15:00:29 -0400

    Add files via upload

--- carrito/restar_stock.php
+++ carrito/restar_stock.php
@@ -1,7 +1,28 @@
 <?php
 session_start();
-$conexion=new mysqli('localhost','root','','implantacion');
+//$conexion=new mysqli('localhost','root','','implantacion');
+include("../includes/db.php");
+
+$connect=new db();
+$conexion=$connect->conexion();
 $id_usuario=$_SESSION['id'];
+$factura=$_SESSION['factura'];
+
+
+$query_sentencia=$conexion->query("SELECT count(id_venta) + 1 as suma_venta  from ventas");
+$row=$query_sentencia->fetch_assoc();
+
+	
+//echo $row['suma_venta'];
+
+
+$suma_venta=$row['suma_venta'];
+define('suma_venta', $suma_venta);
+$constante=suma_venta;
+
+
+define('factura', $_SESSION['factura']);
+$factura=factura;
 
 
 
@@ -20,7 +41,7 @@ foreach ($_SESSION['cart'] as $key => $value) {
 	if ($query){
 
 
-     $sql1="INSERT INTO ventas(Id_producto,Nombre_producto,cantidad,Precio,Precio_dolares,Id_usuario) VALUES ('$id','$Nombre_producto','$cantidad','$precio','$Precio_dolares','$id_usuario')";
+     $sql1="INSERT INTO ventas(Id_venta,Id_producto,Numero_factura,Nombre_producto,cantidad,Precio,Precio_dolares,Id_usuario) VALUES ($constante,'$id','$factura','$Nombre_producto','$cantidad','$precio','$Precio_dolares','$id_usuario')";
 		$query1=$conexion->prepare($sql1);
 		$query1->execute();
 		
@@ -32,7 +53,8 @@ foreach ($_SESSION['cart'] as $key => $value) {
 
         	    window.location.href='cart.php'</script>";
 
-        	    unset($_SESSION['cart']);
+        	    unset($_SESSION['cart']); 
+        	    unset($_SESSION['factura']); 
         }else{
         	echo "ha dado error";
         }
