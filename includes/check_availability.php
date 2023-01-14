69541328a48f5e09711d8e159337bc5d521b6dbb
Victor C <74551928+victorcamacaro253@users.noreply.github.com>
2023-01-14 14:45:01 -0400

    Add files via upload

--- includes/check_availability.php
+++ includes/check_availability.php
@@ -1,6 +1,10 @@
 <?php 
 
-$conexion=new mysqli('localhost','root','','implantacion');
+//$conexion=new mysqli('localhost','root','','implantacion');
+include("../includes/db.php");
+
+$connect=new db();
+$conexion=$connect->conexion();
 
 if (!empty($_POST['oldpassword'])) {
 	$pass=md5($_POST['oldpassword']);
